171b70cd5fd78a400e08d2b4c7ba415d38ae6992
lloyd4565 <88751177+lloyd4565@users.noreply.github.com>
2021-12-15 00:23:42 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 pkg upgrade
-pkg update
+pkg update 
+pkg install figlet
 pkg install nodejs -y
-npm install
+npm install 
+figlet "LIL MANIAC'S ECONOMY"
 echo "LLOYD IS THE BEST, AND ENJOY MY FORK OF ECONOMY"
